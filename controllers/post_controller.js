72ad4c3a218f9a1724a3439664826f90c2f9ba5b
Pradeep Agrawal <pradeepagrawal79@gmail.com>
2020-02-21 17:02:39 +0530

    Update a post api

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -29,5 +29,19 @@ exports.create = (req, res, next) => {
     image: req.body.image
   })
   post.save();
-  res.send('Post added successfully');
+  res.status(201).send('Post added successfully');
+}
+
+exports.update = (req, res, next) => {
+  Post.findById(req.params.id)
+    .then(result => {
+      result.title = req.body.title;
+      result.description = req.body.description;
+      result.image = req.body.image;
+      result.save();
+    }).then(() => {
+      res.status(204).send('post updated successfully');
+    }).catch(err => {
+      res.status(400).send(err);
+    })
 }
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -6,5 +6,6 @@ router.get('/', postController.welcome);
 router.get('/posts', postController.index);
 router.post('/posts', postController.create);
 router.get('/posts/:id', postController.show);
+router.patch('/posts/:id', postController.update);
 
 module.exports = router;
